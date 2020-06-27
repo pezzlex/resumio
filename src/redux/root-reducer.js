a866d4ef2fed7b83a950a32294cfe261547b74d2
Pezanne Khambatta <pkhambat@risd.edu>
2020-06-26 20:20:06 -0700

    minor @iso related bug fixed

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,10 +1,9 @@
-import { combineReducers } from "redux"
 import App from "@iso/redux/app/reducer"
 import Auth from "@iso/redux/auth/reducer"
-import LanguageSwitcher from "@iso/redux/languageSwitcher/reducer"
+
+import { combineReducers } from "redux"
 
 export default combineReducers({
   Auth,
   App,
-  LanguageSwitcher,
 })
