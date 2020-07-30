84043c50501c041affc4e8dbe14a2498ea5214a7
Pezanne Khambatta <pkhambat@risd.edu>
2020-07-30 00:51:33 -0700

    bug removed

--- packages/isomorphic-boilerplate-single/src/redux/root-reducer.js
+++ packages/isomorphic-boilerplate-single/src/redux/root-reducer.js
@@ -1,12 +1,10 @@
 import App from '@iso/redux/app/reducer'
 import Auth from '@iso/redux/auth/reducer'
 import resumeData from './resumes/reducer'
-import templateData from './templates/reducer'
 import { combineReducers } from 'redux'
 
 export default combineReducers({
   Auth,
   App,
   resumeData,
-  templateData,
 })
