ec9a9a9a7a343eba1a641384b639c9423eca8cb5
Pezanne Khambatta <pezanne_khambatta@brown.edu>
2021-01-23 21:08:45 -0500

    Jakes resume link updated

--- src/containers/Templates.js
+++ src/containers/Templates.js
@@ -4,6 +4,7 @@ import { useRouteMatch } from 'react-router-dom'
 import LayoutContent from '../components/utility/layoutContent'
 import LayoutContentWrapper from '../components/utility/layoutWrapper'
 import { Header, Title } from './AppLayout.style'
+import Src from '../assets/images/jakes-resume.jpeg'
 
 const { Text } = Typography
 
@@ -25,10 +26,7 @@ const Templates = () => {
         </Header>
         <Row gutter={12}>
           <Col xl={3} lg={3} md={3} span={24}>
-            <Image
-              width={300}
-              src="https://writelatex.s3.amazonaws.com/published_ver/16158.jpeg?X-Amz-Expires=14400&X-Amz-Date=20210123T202753Z&X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAWJBOALPNITA4UZ5V/20210123/us-east-1/s3/aws4_request&X-Amz-SignedHeaders=host&X-Amz-Signature=0be41392b34a8fcb4a139e5d1dde43636bbbd1f2d9a3c64638003c74060ba437"
-            />
+            <Image width={300} src={Src} />
             <Text> Overleaf: Jake's Resume</Text>
           </Col>
         </Row>
